c208a4ba8a70c689261018bc78420c73a2643b84
FISMATEMATICA <fismat@usp.br>
2026-04-06 10:47:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const nomesDestacados = [
       "L. R. Abramo","O. J. P. Éboli","Bárbara Amaral","R. Zukanovich Funchal",
       "Gabriel Santos Menezes","Gabriel Menezes","Ricardo Correa da Silva","R Correa da Silva",
       "RC da Silva","Lua F. T. Airoldi","Victor Hugo M. Ramos","Victor Hugo Marques Ramos",
-      "Cleverson Andrade Goulart","Mauricio Porto Pato","J. Frenkel", "Andre Lessa"
+      "Cleverson Andrade Goulart","Mauricio Porto Pato","J. Frenkel", "Andre Lessa", "M. Martines"
 ];
 
 const resultsDiv = document.getElementById("results");
